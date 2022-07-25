3e336dc8c2513be3c83aa7cd19a2630ebf39927f
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-07-25 20:59:43 +0530

    refactor: added css and image as an asset

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,15 @@ const webpackConfig = {
 						presets: ["@babel/preset-env"]
 					}
 				}
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource"
 			}
 		]
 	},
